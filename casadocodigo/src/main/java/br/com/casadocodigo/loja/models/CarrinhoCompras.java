ab3e221599b4c901daefca15dcbb185f8117a678
Everton Custodio <evertonhenrique8@gmail.com>
2019-02-13 19:58:57 -0200

    Atividade 03

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.models;
 
+import java.math.BigDecimal;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -27,5 +29,13 @@ public class CarrinhoCompras {
 	
 	public int getQuantidade() {
 		return itens.values().stream().reduce(0, (proximo, acumulador) -> proximo + acumulador);
+	}
+	
+	public Collection<CarrinhoItem> getItens() {
+		return itens.keySet();
+	}
+	
+	public BigDecimal getTotal(CarrinhoItem item) {
+		return item.getTotal(getQuantidade(item));
 	}
 }
